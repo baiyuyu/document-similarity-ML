b076b7cdf98d553d931fd4a923ee805cf8e02234
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-07-09 15:41:11 +0530

    cosine similarity added

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import string
 import pandas as pd
 
 # corpus of four documents
-docs = ["Sachin is considered to be one of the greatest cricket players",
-        "Federer is considered one of the greatest tennis players",
-        "Nadal is considered one of the greatest tennis players",
-        "Virat is the captain of the  Indian cricket team"
+docs = ["Any liability, loss, cost or expense directly or indirectly arising out of, resulting from, caused or contributed to by Genetically Modified Organisms.",
+        "For the purposes of this exclusion the term Genetically Modified Organisms (GMO’s) shall mean and include:",
+        "In the event that the definition of GMO under the applicable laws and/or official regulations relating to genetic engineering or modification in an",
+        "Any liability, loss, cost or expense directly or indirectly arising out of, resulting from, caused or contributed to by Genetically"
 
         ]
 
@@ -54,4 +54,44 @@ idfDict={}
 
 for column in docTermDf.columns:
     idfDict[column]= np.log((len(docs) +1 )/(1+ (docTermDf[column] != 0).sum()))+1
+    
+    
+#compute tf.idf matrix
+docsTfIdfMat = np.zeros((len(docs),len(vocab)))
+docTfIdfDf = pd.DataFrame(docsTfIdfMat ,columns=sorted(vocab.keys()))
+
+
+
+docCount = 0
+for doc in docs:
+    for key in idfDict.keys():
+        docTfIdfDf[key][docCount] = docTermDf[key][docCount] * idfDict[key]
+    docCount = docCount +1 
+    
+print(docTfIdfDf)
+
+
+## Use TfidfVectorizer to perfom the same
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+vectorizer = TfidfVectorizer(analyzer='word',norm=None, use_idf=True,smooth_idf=True)
+tfIdfMat  = vectorizer.fit_transform(docs)
+
+feature_names = sorted(vectorizer.get_feature_names())
+
+docList=['Doc 1','Doc 2','Doc 3','Doc 4']
+skDocsTfIdfdf = pd.DataFrame(tfIdfMat.todense(),index=sorted(docList),  columns=feature_names)
+print(skDocsTfIdfdf)
+
+#compute cosine similarity
+csim = cosine_similarity(tfIdfMat,tfIdfMat)
+
+csimDf = pd.DataFrame(csim,index=sorted(docList),columns=sorted(docList))
+
+
+
+
     
